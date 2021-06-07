1ac82e042db9f5251a664e39edde3f5b655cf53c
luaferraz <moonwitherz@gmail.com>
2021-06-07 14:20:41 -0300

    add all

---
+++ 01 - CalculadoraREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Controllers.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Controllers.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        object Create(PersonController person);
+        object Update(PersonController person);
+    }
+}
---
+++ 01 - CalculadoraREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,79 @@
+﻿using RestWithASPNETUdemy.Controllers.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i ++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Leandro",
+                LastName = "Costa",
+                Address = "Sei lá",
+                Gender = "Male"
+            };
+        }
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Leandro" + i,
+                LastName = "Costa" + i,
+                Address = "Sei lá" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        public object Create(PersonController person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Update(PersonController person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
